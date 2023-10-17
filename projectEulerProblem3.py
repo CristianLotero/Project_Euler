04e91b6e9c160e484488acfb222fc2321f47bc05
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-10-17 00:19:54 -0300

    actualizacion 16/10/23 noche

---
+++ projectEulerProblem3.py
@@ -0,0 +1,35 @@
+# The prime factors of 13195 are 5, 7, 13 and 29.
+
+# What is the largest prime factor of the number 600851475143?
+
+numX = 100
+# numX = 13195
+
+from array import array
+primosArray = array('i')
+
+primosArray.append(2)
+
+def LargestPrime(numX):
+    target = numX
+    i = 2
+    j = 2
+    for i in target - 1:
+        for j in i - 1:
+            if i%j == 0:
+                break
+            elif j == i -1:
+                primosArray.append(i)
+    
+    primosArray.reverse()
+    
+for element in primosArray:
+    if numX%element == 0:
+        largestPrime = element
+
+print(largestPrime)
+print(primosArray)
+print(numX)
+
+
+
