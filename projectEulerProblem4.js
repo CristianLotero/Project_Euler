78f014ea64ebe9e8b6eadedaf9d98de63f6fded9
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-10-22 10:49:11 -0300

    actualizacion 22/10/23 maniana

--- projectEulerProblem4.js
+++ projectEulerProblem4.js
@@ -2,9 +2,23 @@
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
 (function palindrome3digits() {
-    let test1 = 93339;
-    let numToArray = test1.toString();
-    
+    // for (i=999 ; i > 99 ; i--) {
+    //     for (j=999; j > 99 ; j--) {
+    //         result = i * j;
+    //         console.log(`el resultado de ${i} x ${j} es: ${result}`)
+    //     }
+    // }
+
+    let result = 9335339;
+    let resultString = result.toString();
+    let resultArray = Array.from(resultString);
+    let resultArray1 = resultArray.slice(resultArray.length/2);
+    let resultArray2 = resultArray.slice(-(resultArray.length/2));
+
+
+    console.log(resultArray1);
+    console.log(resultArray2);
+    // console.log(typeof(resultArray))
  })();
 
 
