a34deb7545e9ff4046e9cc2b2429a5fe1872780f
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-10-23 03:42:18 -0300

    actualizacion2 23/10/23 madrugada

--- projectEulerProblem4.js
+++ projectEulerProblem4.js
@@ -1,27 +1,27 @@
 // A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
 // Find the largest palindrome made from the product of two 3-digit numbers.
+let result= 0;
+let resultArray;
+let resultArrayReversed;
 
 let breakpoint = 0;
 (function palindrome3digits() {
-    for (i=999 ; i > 99 ; i--) {
-        for (j=999; j > 99 ; j--) {
-            if (breakpoint == 1){
-                break;
-            }
-            let result = i * j;
-            let resultArray = result.toString().split("");
-            for (k=0 ; k < resultArray.length; k++) {
-                if (resultArray[k] == resultArray[((resultArray.length-1)-k)]) {
-                    console.log(`${result} parece palindromo coincidencias ${k}`)
-                } else {
-                    console.log(`${result} no es palindromo`)
-                    break;
+    for (let i=999 ; i > 99 ; i--) {
+        for (let j=999; j > 99 ; j--) {
+            result = i * j;
+            resultArray = result.toString().split("");
+            // resultCopy = resultArray;
+            resultArrayReversed = [...resultArray].reverse();
+            console.log(`RESULTADO: ${result} de ${i} * ${j}`)
+            // console.log(`RESULT ARRAY: ${resultArray}`)
+            // console.log(`REVERSED: ${resultArrayReversed}`)
+            let isEqual = resultArray.toString() === resultArrayReversed.toString();
+            if (isEqual) {
+                    console.log(`El palindromo mas grande es ${resultArrayReversed} producto de ${i} * ${j}`)
+                    return;
                 }
             }
-            console.log(`el palindromo mas grande es: ${result} de ${i} x ${j}`);
-            breakpoint = 1;
         }
-    }
-})();
+    })();
 
 
