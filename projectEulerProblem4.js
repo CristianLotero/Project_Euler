9aa1cf1a4af89662072ab43fabbc55e997b394ec
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-10-26 02:21:25 -0300

    actualizacion 26/10/23 madrugada

--- projectEulerProblem4.js
+++ projectEulerProblem4.js
@@ -1,5 +1,6 @@
 // A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 * 99.
 // Find the largest palindrome made from the product of two 3-digit numbers.
+
 let result= 0;
 let resultArray;
 let resultArrayReversed;
@@ -15,14 +16,15 @@ let breakpoint = 0;
             // console.log(`RESULTADO: ${result} de ${i} * ${j}`)
             let isEqual = resultArray.toString() === resultArrayReversed.toString();
             if (isEqual) {
-                    console.log(`El palindromo mas grande por ahora es ${resultArrayReversed} producto de ${i} * ${j}`)
-                    largest = resultArrayReversed.toString() * 1; 
-                    console.log(largest)
-                    largest = largest > result ? largest : result; 
+                    console.log(`encontrado: ${result} producto de ${i} * ${j}`)
+                    findLargest = () => {
+                        return result > largest ? result : largest;
+                    };
+                    largest = findLargest();
                 }
             }
         }
     })();
 
-console.log(largest)
+console.log(`resultado final : ${largest}`)
 
