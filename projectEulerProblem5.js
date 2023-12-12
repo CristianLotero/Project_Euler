0b0cde3d038c742ce39bf96eac9adff9b671ea01
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-12-11 23:10:15 -0300

    acualizacion lunes 11/12/23 noche

--- projectEulerProblem5.js
+++ projectEulerProblem5.js
@@ -3,6 +3,19 @@
 // What is the smallest positive number that is evenly divisible by all of the numbers from 1
 //  to 20?
 
-function pretentiousNumber (start, end) {
-    let pretentious = 0;
+function pretentiousNumber () {
+    let numeroX = 10
+    divisiblePorTodos = false;
+    while (!divisiblePorTodos) {
+        for (let i = 2; i <= 10 ; i++) {
+            if (numeroX % i == 0) {
+                console.log(`${numeroX} es divisible por ${i}`)
+                continue
+            } else {
+                numeroX++
+            }
+        }
+        divisiblePorTodos = true;
+    }
+    console.log(`Terminé con numeroX =${numeroX}`)
 }
