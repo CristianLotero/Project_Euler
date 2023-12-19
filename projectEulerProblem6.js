016731753091a6620193e3f4fbd8ab007c3b42a4
Dell-de-GureNn <cristianlotero85@gmail.com>
2023-12-19 00:21:41 -0300

    actualizacion3 lunes 18/12/23 noche

--- projectEulerProblem6.js
+++ projectEulerProblem6.js
@@ -5,16 +5,36 @@
 // Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640.
 // Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
-let arrSumSquare = [];
 let arrSquareSum = [];
+let arrSumSquare = [];
+
+let SumSquareResult = 0;
+let squareSumResult = 0;
+let difference = 0;
 
 function sumSquarePush () {
-    for(i=1 ; i <= 10 ; i++) {
-        arrSumSquare.push(i**2)
+    for(i=1 ; i <= 100 ; i++) {
+        arrSquareSum.push(i**2)
     }
-    console.log(arrSumSquare)
-    console.log()
+    console.log(`El array de suma de cuadrados es: ${arrSquareSum}`)
+    SumSquareResult = arrSquareSum.reduce((total, item)=> total + item);
+    console.log(`el resultado de la suma de cuadrados es: ${SumSquareResult}`);
+}
+
+function squareSumPush() {
+    for (i=1; i <= 100 ; i++){
+        arrSumSquare.push(i);
+    }
+    let sumSquareTotal = arrSumSquare.reduce((total, item)=> total + item);
+    console.log(`El array del cuadrado de la suma es: ${arrSumSquare}`)
+    squareSumResult = sumSquareTotal**2;
+    console.log(`el resultado del cuadrado de la  suma es: ${squareSumResult}`);
 }
 function difSumSquares () {
-
+    difference =  squareSumResult - SumSquareResult;
+    console.log(difference)
 }
+
+sumSquarePush();
+squareSumPush();
+difSumSquares();
