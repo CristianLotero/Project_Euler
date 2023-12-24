caad6b267e88c6bba53d9cb63b45815233583e78
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-12-23 23:52:11 -0300

    actualizacion sabado 23/12/23 noche

--- projectEulerProblem7.js
+++ projectEulerProblem7.js
@@ -1,15 +1,21 @@
 // By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 // What is the 10001st prime number?
 
-let aPrimeArray = [];
-let nNPrimeIndex = 0;
-
-console.log(typeof(nNPrimeIndex))
+let primeArray = [2,3,5,7,11];
+let NPrimeIndex = 10;
 
 
-function fParsePrimes () {
-    let nCheckIfXIsPrime = 2;
-    while (aPrimeArray.length !== nNPrimeIndex) {
-        
+function parsePrimes () {
+    while (primeArray.length !== NPrimeIndex){
+        let numberToParse = 12;
+        for (i=2 ; i < numberToParse ; i++) {
+            if (numberToParse%i === 0) {
+                //...
+            } else if (i == numberToParse) {
+                //...
+            }
+        }
     }
 }
+
+console.log(primeArray);
