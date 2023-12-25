de142045e8c9d86d956e47f89efc9556a5c65555
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-12-25 16:10:52 -0300

    actualizacion lunes 25/12/23 tarde

--- projectEulerProblem7.js
+++ projectEulerProblem7.js
@@ -2,15 +2,25 @@
 // What is the 10001st prime number?
 
 let primeArray = [2,3,5,7,11];
-let NPrimeIndex = 10;
+let NPrimeIndex = 10001;
 
 let numberToParse = 12;
 
 function parsePrimes () {
     while (primeArray.length !== NPrimeIndex){
-        
+        console.log(numberToParse)
+        if (numberToParse%2 === 0 || numberToParse%3 === 0 || numberToParse%5 === 0 || numberToParse%7 === 0) {
+            numberToParse += 1;
+        } else {
+            const primeOrNor = primeArray.some((element) => numberToParse%element === 0);
+            if (!primeOrNor) {
+                primeArray.push(numberToParse);
+            }
+            numberToParse += 1;
+        }
     }
 }
 
 parsePrimes();
-console.log(primeArray);
+console.log(`Este es el array de números primos al finalizar: ${primeArray}`);
+console.warn(`Este es el 10001st número primo ${primeArray[10000]}`);
