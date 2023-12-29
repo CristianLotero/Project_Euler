2144a6dfa372137d82d834012c31fe9af007a710
Cristian Lotero Aquino <cristianlotero85@gmail.com>
2023-12-28 23:00:16 -0300

    actualizado jueves 28/12/23 noche

--- projectEulerProblem9.js
+++ projectEulerProblem9.js
@@ -9,18 +9,22 @@
 // Find the product abc.
 
 function pythagoreanTriplet () {
-    for (k=6, j=5, i=4; i+j+k <= 1000 ; k++ ) {
-        for (; j < k; j++ ){
-            for(; i**2 + j**2 <= k**2 && i < j ; i++) {
+    for (k=3, j=2, i=1; i+j+k <= 1000 ; k++ ) {
+        for (j=2; j < k; j++ ){
+            for(i=1; i**2 + j**2 <= k**2 && i < j ; i++) {
                 if (i**2+j**2 === k**2) {
                     console.log(`La tripleta pitagorea (${i}² = ${i**2}) +  (${j}² = ${j**2}) = ${i**2 + j**2} =  ${k}² = ${k**2} fue encontrada`)
-                }
-                if (i+j+k === 1000) {
-                console.warn(`encontrado!!!  (${i}² = ${i**2}) +  (${j}² = ${j**2}) = ${i**2 + j**2} =  ${k}² = ${k**2}`)
+                    if (i+j+k === 1000) {
+                    console.warn(`encontrado!!!  ${i} + ${j} + ${k} = ${i+j+k }`)
+                    a=i;
+                    b=j;
+                    c=k;
+                    }
                 }
             }
         }
-    }    
+    }
+    console.log (`el resultado del problema 9 de Project Euler es ${a*b*c}  `)
 }
 
 pythagoreanTriplet();
